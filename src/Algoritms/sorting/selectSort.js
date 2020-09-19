a1b888602693401d682f50fd1daaf47c6fa17c69
Aliaksandr <sasha.klim.2017@inbox.ru>
2020-09-19 19:20:14 +0300

    add selectSort

---
+++ src/Algoritms/sorting/selectSort.js
@@ -0,0 +1,20 @@
+const list = [4, 7, 1, 5, 9, 0, 3, 2, 8, 6];
+
+function selectSort(list) {
+  for (let i = 0; i < list.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < list.length; j++) {
+      if (list[j] < list[min]) {
+        min = j;
+      }
+    }
+    let dummy = list[i];
+    list[i] = list[min];
+    list[min] = dummy;
+    // Три строчки выше можно заменить на одну 
+    // [list[i], list[min]] = [list[min], list[i]];
+  }
+  return list;
+}
+
+console.log(selectSort(list));
