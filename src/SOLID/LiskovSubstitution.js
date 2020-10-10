2791abfd6dc8da11cb7c52597da3dcb260c246ec
Aliaksandr <sasha.klim.2017@inbox.ru>
2020-10-10 20:30:59 +0300

    add Liskov Substitution

---
+++ src/SOLID/LiskovSubstitution.js
@@ -0,0 +1,19 @@
+class Animal {
+  LegCount(){};
+}
+
+class Lion extends Animal {
+  LegCount() {
+    return 'Lion has 4 legs';
+  }
+}
+
+const animal = [new Lion];
+
+const countLegs = (animals) => {
+  animals.forEach(animal => {
+    console.log(animal.LegCount());
+  });
+}
+
+countLegs(animal)
